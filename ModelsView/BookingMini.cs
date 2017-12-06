224ed5e0e9ce099c0868368b5ed4bac6ce1f4fcc
Jerome Siddiqi <jerosid@gmail.com>
2017-12-06 12:36:29 +0000

    Add files via upload

---
+++ ModelsView/BookingMini.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ThirdPartyDbWebAPI.ModelsView
+{
+
+    public class BookingMini
+    {
+
+        // do not add methods or constructors because it will corrupt the API
+
+        public string FullReference { get; set; }
+
+        public string Status { get; set; }
+
+        public string Consultant { get; set; }
+
+        public DateTime Date_Entered { get; set; }
+
+        public string Sales_Update { get; set; }
+        public int? BHD_ID { get; set; }
+
+    }
+}
